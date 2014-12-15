43e6b41169cf14f086dc48974cc4bacaa7ba9399
Nalin Chopra <nalinchopra@Nalins-MacBook-Air.local>
2014-12-15 12:16:45 -0800

    add migration for user and add user model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define("user", {
+    email: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    password_digest: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+
+  return user;
+};
